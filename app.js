34efcf45f6aa6b97ba3c40ac2842ee50a59936a1
Husam-Zabian <husamthubian99@yahoo.com>
2023-02-14 20:23:47 +0000

    lab08V1

--- app.js
+++ app.js
@@ -46,52 +46,107 @@ const valorant7 = new Employee(1, "Hadi Ahmad", "Finance", "Mid-Senior", "testin
 
 
 
-valorant1.calculatingSalary();
-valorant1.render();
-console.log(valorant1);
+//valorant1.calculatingSalary();
+//valorant1.render();
+//console.log(valorant1);
 
-valorant2.calculatingSalary();
-valorant2.render();
-console.log(valorant2);
+//valorant2.calculatingSalary();
+//valorant2.render();
+//console.log(valorant2);
 
-valorant3.calculatingSalary();
-valorant3.render();
-console.log(valorant3);
+//valorant3.calculatingSalary();
+//valorant3.render();
+//console.log(valorant3);
 
-valorant4.calculatingSalary();
-valorant4.render();
-console.log(valorant4);
+//valorant4.calculatingSalary();
+//valorant4.render();
+//console.log(valorant4);
 
-valorant5.calculatingSalary();
-valorant5.render();
-console.log(valorant5);
+//valorant5.calculatingSalary();
+//valorant5.render();
+//console.log(valorant5);
 
-valorant6.calculatingSalary();
-valorant6.render();
-console.log(valorant6);
+//valorant6.calculatingSalary();
+//valorant6.render();
+//console.log(valorant6);
 
-valorant7.calculatingSalary();
-valorant7.render();
-console.log(valorant7);
-
-
+//valorant7.calculatingSalary();
+//valorant7.render();
+//console.log(valorant7);
 
 
 
 
 
 
-//let totalSalary = finalresult * tax;
+
+
 
 const test = new Employee("Husam", "Zabian", "Developer", "Senior", "aaa");
 
-//console.log(test);
 
 
 
+let res =0
+
+function uniqueID() {
+ // res = Math.floor((Math.random()) * 9000 + 1000) 
+ res = Date.now()
+ res = res.toString()
+ return  res = res.substr(-4);
+  }
+
+
+ uniqueID();
+
+console.log(res);
 
 
 
+Employee.prototype.render = function() {
+
+    
+
+    const container = document.getElementById('Card');
+    
+
+    const divEl = document.createElement('div');
+    container.appendChild(divEl);
+
+    
+    const nameEl = document.createElement('h3');
+    divEl.appendChild(nameEl);
+    nameEl.textContent = this.FullName;
+
+    
+    const priceEl = document.createElement('h5');
+    divEl.appendChild(priceEl);
+    priceEl.textContent = `Salary: ${this.Salary}$`
+
+
+    
+    const imgEl = document.createElement('img');
+    divEl.appendChild(imgEl);
+    imgEl.setAttribute('src',this.imgPath);
+    imgEl.width = "150";
+    imgEl.height = "150";
+
+    const pEl = document.createElement('p');
+    divEl.append(pEl);
+
+    
+    
+}
 
 
 
+drinkForm.addEventListener('submit', addNewEmployeeHandler);
+
+function addNewEmployeeHandler(event) {
+    event.preventDefault();
+    console.log(event);
+    let drinkName = event.target.name.value;
+    let ingredients = event.target.ingredients.value;
+    let imgPath = event.target.imgUrl.value;
+    
+}
