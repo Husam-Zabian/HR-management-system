1fa13cd8d55e5cd6fe53f6b06bb2000e45d23b67
Husam-Zabian <husamthubian99@yahoo.com>
2023-03-23 10:39:00 +0000

    lab09 done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+let EmployeeArr = [];
 function Employee(EmployeeId, FullName, Department, Level, ImageURL, Salary) {
   this.EmployeeId = EmployeeId;
   this.FullName = FullName;
@@ -5,63 +7,73 @@ function Employee(EmployeeId, FullName, Department, Level, ImageURL, Salary) {
   this.Level = Level;
   this.ImageURL = ImageURL;
   this.Salary = Salary;
+  EmployeeArr.push(this);
 }
 
 Employee.prototype.calculateSalary = function () {
   if (this.Level == "Senior") {
-      this.Salary = Math.floor(Math.random() * 500 + 1500);
-      this.Salary = this.Salary - (this.Salary * 0.075);
+    this.Salary = Math.floor(Math.random() * 500 + 1500);
+    this.Salary = this.Salary - (this.Salary * 0.075);
 
   }
   else if (this.Level == "Mid-Senior") {
-      this.Salary = Math.floor(Math.random() * 500 + 1000);
-      this.Salary = this.Salary - (this.Salary * 0.075);
+    this.Salary = Math.floor(Math.random() * 500 + 1000);
+    this.Salary = this.Salary - (this.Salary * 0.075);
   }
   else if (this.Level == "Junior") {
-      this.Salary = Math.floor(Math.random() * 500 + 500);
-      this.Salary = this.Salary - (this.Salary * 0.075);
+    this.Salary = Math.floor(Math.random() * 500 + 500);
+    this.Salary = this.Salary - (this.Salary * 0.075);
   }
 };
 
-Employee.prototype.render = function () {
+function render() {
+
   let divStile = document.getElementById('employee-container');
+  divStile.innerHTML = '';
 
-  const divEl = document.createElement('div');
-  divEl.setAttribute('class', "employee-card");
-  divStile.appendChild(divEl);
+  getEmployee();
 
+  if (EmployeeArr == null) {
+    EmployeeArr = [];
+  }
+  for (let i = 0; i < EmployeeArr.length; i++) {
 
-  //display the Employee img
-  const imgEl = document.createElement('img');
-  divEl.appendChild(imgEl);
-  imgEl.setAttribute('src', this.ImageURL);
-  imgEl.setAttribute('alt', "Employee Image");
-
-  //display Name and Id 
-
-
-  const nameIdEl = document.createElement('p');
-  divEl.appendChild(nameIdEl);
-  nameIdEl.textContent = `Name: ${this.FullName}`;
-
-  const nameIdE2 = document.createElement('p');
-  divEl.appendChild(nameIdE2);
-  nameIdE2.textContent = `Id: ${this.EmployeeId}`;
-
-  //display if the drink is hot and/or cold
-  const pEl = document.createElement('p');
-  divEl.append(pEl);
-  pEl.textContent = `Department: ${this.Department}`;
-
-  const pE2 = document.createElement('p');
-  divEl.append(pE2);
-  pE2.textContent = `Level: ${this.Level}`;
-
-  const pE3 = document.createElement('p');
-  divEl.append(pE3);
-  pE3.textContent = `Salary: ${this.Salary}`;
+    const divEl = document.createElement('div');
+    divEl.setAttribute('class', "employee-card");
+    divStile.appendChild(divEl);
 
 
+
+    const imgEl = document.createElement('img');
+    divEl.appendChild(imgEl);
+    imgEl.setAttribute('src', EmployeeArr[i].ImageURL);
+    imgEl.setAttribute('alt', "Employee Image");
+
+
+    const nameIdEl = document.createElement('p');
+    divEl.appendChild(nameIdEl);
+    nameIdEl.textContent = `Name: ${EmployeeArr[i].FullName}`;
+
+    const nameIdE2 = document.createElement('p');
+    divEl.appendChild(nameIdE2);
+    nameIdE2.textContent = `Id: ${EmployeeArr[i].EmployeeId}`;
+
+
+    const pEl = document.createElement('p');
+    divEl.append(pEl);
+    pEl.textContent = `Department: ${EmployeeArr[i].Department}`;
+
+    const pE2 = document.createElement('p');
+    divEl.append(pE2);
+    pE2.textContent = `Level: ${EmployeeArr[i].Level}`;
+
+    const pE3 = document.createElement('p');
+    divEl.append(pE3);
+    pE3.textContent = `Salary: ${EmployeeArr[i].Salary}`;
+
+
+
+  }
 };
 
 
@@ -69,8 +81,8 @@ var EmployeesIdes = new Array();
 
 function generateEmployeeId(existingEmployees) {
   const id = Math.floor(Math.random() * 9000) + 1000;
-  if (existingEmployees.includes(id)) { // If the number is already in use, generate a new one
-      return generateEmployeeId(existingEmployees);
+  if (existingEmployees.includes(id)) {
+    return generateEmployeeId(existingEmployees);
   }
   EmployeesIdes.push(id);
   return id;
@@ -95,8 +107,21 @@ function addNewEmployeeFormHandler(event) {
 
   let newEmployee = new Employee(generatedEmployeeId, employeeFullName, departmentValue, levelValue, employeeImageURL, 0);
   newEmployee.calculateSalary();
-  newEmployee.render();
+
+  let jsonArr = JSON.stringify(EmployeeArr);
+  localStorage.setItem("allEmployee", jsonArr);
+
+  render();
 
   document.getElementById("employeeForm").reset();
-
 }
+
+function getEmployee() {
+  let jsonArr = localStorage.getItem("allEmployee");
+  EmployeeArr = JSON.parse(jsonArr);
+}
+
+getEmployee();
+render();
+
+
