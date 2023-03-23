748202b3f24259881373248f4b9eb3a55bcca114
Husam-Zabian <husamthubian99@yahoo.com>
2023-03-23 10:04:04 +0000

    lb08 Done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HR-management-system
+
+# index html now could add an employee card
+
+# app js now are appending a new card from my constructor
--- app.js
+++ app.js
@@ -1,152 +1,102 @@
-'use strict';
-
-
-function Employee(EmployeeID, FullName, Department, Level, ImageURL) {
-  this.EmployeeID = EmployeeID;
+function Employee(EmployeeId, FullName, Department, Level, ImageURL, Salary) {
+  this.EmployeeId = EmployeeId;
   this.FullName = FullName;
   this.Department = Department;
   this.Level = Level;
   this.ImageURL = ImageURL;
+  this.Salary = Salary;
+}
+
+Employee.prototype.calculateSalary = function () {
+  if (this.Level == "Senior") {
+      this.Salary = Math.floor(Math.random() * 500 + 1500);
+      this.Salary = this.Salary - (this.Salary * 0.075);
+
+  }
+  else if (this.Level == "Mid-Senior") {
+      this.Salary = Math.floor(Math.random() * 500 + 1000);
+      this.Salary = this.Salary - (this.Salary * 0.075);
+  }
+  else if (this.Level == "Junior") {
+      this.Salary = Math.floor(Math.random() * 500 + 500);
+      this.Salary = this.Salary - (this.Salary * 0.075);
+  }
+};
+
+Employee.prototype.render = function () {
+  let divStile = document.getElementById('employee-container');
+
+  const divEl = document.createElement('div');
+  divEl.setAttribute('class', "employee-card");
+  divStile.appendChild(divEl);
+
+
+  //display the Employee img
+  const imgEl = document.createElement('img');
+  divEl.appendChild(imgEl);
+  imgEl.setAttribute('src', this.ImageURL);
+  imgEl.setAttribute('alt', "Employee Image");
+
+  //display Name and Id 
+
+
+  const nameIdEl = document.createElement('p');
+  divEl.appendChild(nameIdEl);
+  nameIdEl.textContent = `Name: ${this.FullName}`;
+
+  const nameIdE2 = document.createElement('p');
+  divEl.appendChild(nameIdE2);
+  nameIdE2.textContent = `Id: ${this.EmployeeId}`;
+
+  //display if the drink is hot and/or cold
+  const pEl = document.createElement('p');
+  divEl.append(pEl);
+  pEl.textContent = `Department: ${this.Department}`;
+
+  const pE2 = document.createElement('p');
+  divEl.append(pE2);
+  pE2.textContent = `Level: ${this.Level}`;
+
+  const pE3 = document.createElement('p');
+  divEl.append(pE3);
+  pE3.textContent = `Salary: ${this.Salary}`;
+
+
 };
 
 
+var EmployeesIdes = new Array();
 
-
-Employee.prototype.calculatingSalary = function () {
-  let max = 10;
-  let min = 5;
-  if (this.Level == "Junior") {
-    min = 500;
-    max = 1000;
-  } else if (this.Level == "Mid-Senior") {
-    min = 1000;
-    max = 1500;
-  } else {
-    min = 1500;
-    max = 2000;
+function generateEmployeeId(existingEmployees) {
+  const id = Math.floor(Math.random() * 9000) + 1000;
+  if (existingEmployees.includes(id)) { // If the number is already in use, generate a new one
+      return generateEmployeeId(existingEmployees);
   }
-  let aaaa = Math.floor(Math.random() * (max - min) + min);
-  this.Salary = aaaa - (aaaa * 7.5/100);
-  console.log(aaaa)
-};
-
-Employee.prototype.render = function(){
-  document.write(`Name : ${this.FullName}  Salary : ${this.Salary}    \r`);
+  EmployeesIdes.push(id);
+  return id;
 }
 
+//Events
 
-const valorant1 = new Employee(1, "Ghazi Samer", "Administration", "Senior", "testing");
-const valorant2 = new Employee(1, "Lana Ali", "Finance", "Senior", "testing");
-const valorant3 = new Employee(1, "Tamara Ayoub", "Marketing", "Senior", "testing");
-const valorant4 = new Employee(1, "Safi Walid", "Administration", "Mid-Senior", "testing");
-const valorant5 = new Employee(1, "Omar Zaid", "Development", "Senior", "testing");
-const valorant6 = new Employee(1, "Rana Saleh", "Development", "Junior", "testing");
-const valorant7 = new Employee(1, "Hadi Ahmad", "Finance", "Mid-Senior", "testing");
+let employeeForm = document.getElementById("employeeForm");
+employeeForm.addEventListener('submit', addNewEmployeeFormHandler);
 
+function addNewEmployeeFormHandler(event) {
 
+  event.preventDefault();
 
+  const employeeFullName = event.target.fullName.value;
+  const department = document.getElementById("department");
+  const departmentValue = department.value;
+  const level = document.getElementById("level");
+  const levelValue = level.value;
+  const employeeImageURL = event.target.imageURL.value;
+  const generatedEmployeeId = generateEmployeeId(EmployeesIdes);
 
-//valorant1.calculatingSalary();
-//valorant1.render();
-//console.log(valorant1);
+  let newEmployee = new Employee(generatedEmployeeId, employeeFullName, departmentValue, levelValue, employeeImageURL, 0);
+  newEmployee.calculateSalary();
+  newEmployee.render();
 
-//valorant2.calculatingSalary();
-//valorant2.render();
-//console.log(valorant2);
+  document.getElementById("employeeForm").reset();
 
-//valorant3.calculatingSalary();
-//valorant3.render();
-//console.log(valorant3);
-
-//valorant4.calculatingSalary();
-//valorant4.render();
-//console.log(valorant4);
-
-//valorant5.calculatingSalary();
-//valorant5.render();
-//console.log(valorant5);
-
-//valorant6.calculatingSalary();
-//valorant6.render();
-//console.log(valorant6);
-
-//valorant7.calculatingSalary();
-//valorant7.render();
-//console.log(valorant7);
-
-
-
-
-
-
-
-
-
-const test = new Employee("Husam", "Zabian", "Developer", "Senior", "aaa");
-
-
-
-
-let res =0
-
-function uniqueID() {
- // res = Math.floor((Math.random()) * 9000 + 1000) 
- res = Date.now()
- res = res.toString()
- return  res = res.substr(-4);
-  }
-
-
- uniqueID();
-
-console.log(res);
-
-
-
-Employee.prototype.render = function() {
-
-    
-
-    const container = document.getElementById('Card');
-    
-
-    const divEl = document.createElement('div');
-    container.appendChild(divEl);
-
-    
-    const nameEl = document.createElement('h3');
-    divEl.appendChild(nameEl);
-    nameEl.textContent = this.FullName;
-
-    
-    const priceEl = document.createElement('h5');
-    divEl.appendChild(priceEl);
-    priceEl.textContent = `Salary: ${this.Salary}$`
-
-
-    
-    const imgEl = document.createElement('img');
-    divEl.appendChild(imgEl);
-    imgEl.setAttribute('src',this.imgPath);
-    imgEl.width = "150";
-    imgEl.height = "150";
-
-    const pEl = document.createElement('p');
-    divEl.append(pEl);
-
-    
-    
-}
-
-
-
-drinkForm.addEventListener('submit', addNewEmployeeHandler);
-
-function addNewEmployeeHandler(event) {
-    event.preventDefault();
-    console.log(event);
-    let drinkName = event.target.name.value;
-    let ingredients = event.target.ingredients.value;
-    let imgPath = event.target.imgUrl.value;
-    
 }
